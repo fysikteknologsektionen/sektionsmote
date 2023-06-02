d028ed3f9e0df997f80d84dd1f649de4612b7088
Josef Jörgensen <josef.jorgensen@hotmail.se>
2023-06-02 18:25:31 +0200

    Added Docker devcontainer

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_10_02_175203) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
